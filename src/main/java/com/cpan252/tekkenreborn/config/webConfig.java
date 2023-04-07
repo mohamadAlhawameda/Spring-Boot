c7d98cd03de5f565d9a12998be7188dba7efba1a
mohamadAlhawameda <alhawameda4@gmail.com>
2023-04-07 03:16:45 -0400

    lab9

--- src/main/java/com/cpan252/tekkenreborn/config/webConfig.java
+++ src/main/java/com/cpan252/tekkenreborn/config/webConfig.java
@@ -3,6 +3,8 @@ package com.cpan252.tekkenreborn.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class webConfig implements WebMvcConfigurer {
@@ -10,5 +12,16 @@ public class webConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("authentication/login");    }
+
+
+     /**
+     * We create a new RestTemplate bean to use for all requests.
+     * It helps us to fetch data from the Tekken User Dashboard API.
+     * @return RestTemplate
+     */
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
 }
